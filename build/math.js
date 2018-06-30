935c0f356bac169c0488d943005ee8dedf6b92be
paulmasson <paulmasson@comcast.net>
2018-06-29 17:28:00 -0700

    Cleanup

--- build/math.js
+++ build/math.js
@@ -283,24 +283,23 @@ function hankel2( n, x ) {
 
 function airyAi( x ) {
 
-  if ( x > 0 ) return 1/pi * sqrt(x/3) * besselK(1/3, 2/3*x**(3/2));
-
   if ( x === 0 ) return 1 / 3**(2/3) / gamma(2/3);
 
-  if ( x < 0 ) return sqrt(-x) / 2 * (  besselJ(1/3, 2/3*(-x)**(3/2))
-                                       - besselY(1/3, 2/3*(-x)**(3/2)) / sqrt(3) );
+  if ( x < 0 ) return sqrt(-x) / 2 * ( besselJ( 1/3, 2/3*(-x)**(3/2) )
+                                       - besselY( 1/3, 2/3*(-x)**(3/2) ) / sqrt(3) );
+
+  return 1/pi * sqrt(x/3) * besselK( 1/3, 2/3*x**(3/2) );
 
 }
 
 function airyBi( x ) {
 
-  if ( x > 0 ) return sqrt(x/3) * ( besselI(1/3, 2/3*x**(3/2))
-                                    + besselI(-1/3, 2/3*x**(3/2)) );
-
   if ( x === 0 ) return 1 / 3**(1/6) / gamma(2/3);
 
-  if ( x < 0 ) return -sqrt(-x) / 2 * ( besselJ(1/3, 2/3*(-x)**(3/2)) / sqrt(3)
-                                        + besselY(1/3, 2/3*(-x)**(3/2)) );
+  if ( x < 0 ) return -sqrt(-x) / 2 * ( besselJ( 1/3, 2/3*(-x)**(3/2) ) / sqrt(3)
+                                        + besselY( 1/3, 2/3*(-x)**(3/2) ) );
+
+  return sqrt(x/3) * ( besselI( 1/3, 2/3*x**(3/2) ) + besselI( -1/3, 2/3*x**(3/2) ) );
 
 }
 
