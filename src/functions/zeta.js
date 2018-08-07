ec4b2b367959d7176de327b742859c0e312ff90d
paulmasson <paulmasson@comcast.net>
2018-08-07 16:00:43 -0700

    Make zeta complex

--- src/functions/zeta.js
+++ src/functions/zeta.js
@@ -1,9 +1,6 @@
 
 function zeta( x ) {
 
-  // functional equation
-  if ( x < 0 ) return 2**x * pi**(x-1) * sin(pi*x/2) * gamma(1-x) * zeta(1-x);
-
   // Borwein algorithm
 
   var n = 14; // from error bound for tolerance of 1e-10 
@@ -18,6 +15,24 @@ function zeta( x ) {
     return n * d;
 
   }
+
+  if ( isComplex(x) ) {
+
+    // functional equation
+    if ( x.re < 0 )
+      return mul( pow(2,x), pow(pi,sub(x,1)), sin( mul(pi/2,x) ), gamma( sub(1,x) ), zeta( sub(1,x) ) );
+
+    var s = complex(0);
+
+    for ( var k = 0 ; k < n ; k++ )
+      s = add( s, div( (-1)**k * ( d(k) - d(n) ), pow( k+1, x ) ) );
+
+    return div( div( s, -d(n) ), sub( 1, pow( 2, sub(1,x) ) ) );
+
+  }
+
+  // functional equation
+  if ( x < 0 ) return 2**x * pi**(x-1) * sin(pi*x/2) * gamma(1-x) * zeta(1-x);
 
   var s = 0;
 
