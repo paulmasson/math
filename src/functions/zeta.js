42b7d6fc1585a6422ada6c861b75f59677c75c3c
paulmasson <paulmasson@comcast.net>
2018-08-07 15:29:18 -0700

    Improve zeta function

--- src/functions/zeta.js
+++ src/functions/zeta.js
@@ -2,18 +2,31 @@
 function zeta( x ) {
 
   // functional equation
-  if ( x < 0 ) return 2**x * pi**(x-1) * sin(pi*x/2) * gamma(1-x) * zeta(1-x); 
+  if ( x < 0 ) return 2**x * pi**(x-1) * sin(pi*x/2) * gamma(1-x) * zeta(1-x);
 
-  // summation of Dirichlet eta function
-  var s = 0;
-  for ( var i = 1 ; i < 1e7 ; i++ ) {
-    var last = s;
-    s -= (-1)**i / i**x;
-    if ( Math.abs(s-last) < 1e-8 ) break;
+  // Borwein algorithm
+
+  var n = 14; // from error bound for tolerance of 1e-10 
+
+  function d( k ) {
+
+    var d = 0;
+
+    for ( var i = 0 ; i <= k ; i++ )
+      d += 4**i * factorial( n+i-1 ) / factorial( n-i ) / factorial( 2*i );
+
+    return n * d;
+
   }
 
-  return s / ( 1 - 2**(1-x) );
+  var s = 0;
+
+  for ( var k = 0 ; k < n ; k++ )
+    s += (-1)**k * ( d(k) - d(n) ) / (k+1)**x;
+
+  return -s / d(n) / ( 1 - 2**(1-x) );
 
 }
 
+function eta( x ) { return ( 1 - 2**(1-x) ) * zeta(x); }
 
