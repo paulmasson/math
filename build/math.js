8da1949ba58a764ff77989031d397452dee9d274
paulmasson <paulmasson@comcast.net>
2018-08-08 16:23:58 -0700

    Make carlsonRJ complex

--- build/math.js
+++ build/math.js
@@ -516,58 +516,115 @@ function carlsonRG( x, y, z ) {
 
 function carlsonRJ( x, y, z, p, tolerance=1e-10 ) {
 
-  // adapted from mpmath / elliptic.py
+  if ( isComplex(x) || isComplex(y) || isComplex(z) || isComplex(p) ) {
 
-  var xm = x;
-  var ym = y;
-  var zm = z;
-  var pm = p;
+    var xm = x;
+    var ym = y;
+    var zm = z;
+    var pm = p;
 
-  var A0 = Am = (x + y + z + 2*p) / 5;
-  var delta = (p-x) * (p-y) * (p-z);
-  var Q = Math.pow( .25*tolerance, -1/6 )
-          * Math.max( Math.abs(A0-x), Math.abs(A0-y), Math.abs(A0-z), Math.abs(A0-p) );
-  var m = 0;
-  var g = .25;
-  var pow4 = 1;
-  var S = 0;
+    var A0 = Am = div( add( x, y, z, mul(2,p) ), 5 );
+    var delta = mul( sub(p,x), sub(p,y), sub(p,z) );
+    var Q = Math.pow( .25*tolerance, -1/6 )
+            * Math.max( abs( sub(A0,x) ), abs( sub(A0,y) ), abs( sub(A0,z) ), abs( sub(A0,p) ) );
+    var m = 0;
+    var g = .25;
+    var pow4 = 1;
+    var S = complex(0);
 
-  while ( true ) {
-    var sx = Math.sqrt(xm);
-    var sy = Math.sqrt(ym);
-    var sz = Math.sqrt(zm);
-    var sp = Math.sqrt(pm);
-    var lm = sx*sy + sx*sz + sy*sz;
-    var Am1 = (Am + lm) * g;
-    xm = (xm + lm) * g;
-    ym = (ym + lm) * g;
-    zm = (zm + lm) * g;
-    pm = (pm + lm) * g;
-    var dm = (sp+sx) * (sp+sy) * (sp+sz);
-    var em = delta * Math.pow( 4, -3*m ) / dm**2;
-    if ( pow4 * Q < Math.abs(Am) ) break;
-    var T = carlsonRC( 1, 1 + em ) * pow4 / dm;
-    S += T;
-    pow4 *= g;
-    m += 1;
-    Am = Am1;
+    while ( true ) {
+      var sx = sqrt(xm);
+      var sy = sqrt(ym);
+      var sz = sqrt(zm);
+      var sp = sqrt(pm);
+      var lm = add( mul(sx,sy), mul(sx,sz), mul(sy,sz) );
+      var Am1 = mul( add(Am,lm), g );
+      xm = mul( add(xm,lm), g );
+      ym = mul( add(ym,lm), g );
+      zm = mul( add(zm,lm), g );
+      pm = mul( add(pm,lm), g );
+      var dm = mul( add(sp,sx), add(sp,sy), add(sp,sz) );
+      var em = mul( delta, Math.pow( 4, -3*m ), inv(dm), inv(dm) );
+      if ( pow4 * Q < abs(Am) ) break;
+      var T = mul( carlsonRC( 1, add(1,em) ), pow4, inv(dm) );
+      S = add( S, T );
+      pow4 *= g;
+      m += 1;
+      Am = Am1;
+    }
+
+    var t = div( Math.pow( 2, -2*m ), Am );
+    var X = mul( sub(A0,x), t );
+    var Y = mul( sub(A0,y), t );
+    var Z = mul( sub(A0,z), t );
+    var P = div( add(X,Y,Z), -2 );
+    var E2 = add( mul(X,Y), mul(X,Z), mul(Y,Z), mul(-3,P,P) );
+    var E3 = add( mul(X,Y,Z), mul(2,E2,P), mul(4,P,P,P) );
+    var E4 = mul( add( mul(2,X,Y,Z), mul(E2,P), mul(3,P,P,P) ), P );
+    var E5 = mul(X,Y,Z,P,P);
+    P = add( 24024, mul(-5148,E2), mul(2457,E2,E2), mul(4004,E3), mul(-4158,E2,E3), mul(-3276,E4), mul(2772,E5) );
+    Q = 24024;
+    var v1 = mul( g**m / Q, pow( Am, -1.5 ), P );
+    var v2 = mul(6,S);
+
+    return add( v1, v2 );
+
+  } else {
+
+    // adapted from mpmath / elliptic.py
+
+    var xm = x;
+    var ym = y;
+    var zm = z;
+    var pm = p;
+
+    var A0 = Am = (x + y + z + 2*p) / 5;
+    var delta = (p-x) * (p-y) * (p-z);
+    var Q = Math.pow( .25*tolerance, -1/6 )
+            * Math.max( Math.abs(A0-x), Math.abs(A0-y), Math.abs(A0-z), Math.abs(A0-p) );
+    var m = 0;
+    var g = .25;
+    var pow4 = 1;
+    var S = 0;
+
+    while ( true ) {
+      var sx = Math.sqrt(xm);
+      var sy = Math.sqrt(ym);
+      var sz = Math.sqrt(zm);
+      var sp = Math.sqrt(pm);
+      var lm = sx*sy + sx*sz + sy*sz;
+      var Am1 = (Am + lm) * g;
+      xm = (xm + lm) * g;
+      ym = (ym + lm) * g;
+      zm = (zm + lm) * g;
+      pm = (pm + lm) * g;
+      var dm = (sp+sx) * (sp+sy) * (sp+sz);
+      var em = delta * Math.pow( 4, -3*m ) / dm**2;
+      if ( pow4 * Q < Math.abs(Am) ) break;
+      var T = carlsonRC( 1, 1 + em ) * pow4 / dm;
+      S += T;
+      pow4 *= g;
+      m += 1;
+      Am = Am1;
+    }
+
+    var t = Math.pow( 2, -2*m ) / Am;
+    var X = (A0-x) * t;
+    var Y = (A0-y) * t;
+    var Z = (A0-z) * t;
+    var P = (-X-Y-Z) / 2;
+    var E2 = X*Y + X*Z + Y*Z - 3*P**2;
+    var E3 = X*Y*Z + 2*E2*P + 4*P**3;
+    var E4 = ( 2*X*Y*Z + E2*P + 3*P**3 ) * P;
+    var E5 = X*Y*Z*P**2;
+    P = 24024 - 5148*E2 + 2457*E2**2 + 4004*E3 - 4158*E2*E3 - 3276*E4 + 2772*E5;
+    Q = 24024;
+    var v1 = g**m * Math.pow( Am, -1.5 ) * P/Q;
+    var v2 = 6*S;
+
+    return v1 + v2;
+
   }
-
-  var t = Math.pow( 2, -2*m ) / Am;
-  var X = (A0-x) * t;
-  var Y = (A0-y) * t;
-  var Z = (A0-z) * t;
-  var P = (-X-Y-Z) / 2;
-  var E2 = X*Y + X*Z + Y*Z - 3*P**2;
-  var E3 = X*Y*Z + 2*E2*P + 4*P**3;
-  var E4 = ( 2*X*Y*Z + E2*P + 3*P**3 ) * P;
-  var E5 = X*Y*Z*P**2;
-  P = 24024 - 5148*E2 + 2457*E2**2 + 4004*E3 - 4158*E2*E3 - 3276*E4 + 2772*E5;
-  Q = 24024;
-  var v1 = g**m * Math.pow( Am, -1.5 ) * P/Q;
-  var v2 = 6*S;
-
-  return v1 + v2;
 
 }
 
