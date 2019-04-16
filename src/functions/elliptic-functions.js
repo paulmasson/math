8cc42b7bc612db23dc2da7e2c019860ea2df1751
paulmasson <paulmasson@analyticphysics.com>
2019-04-16 12:32:37 -0700

    Add Weierstrass half periods

--- src/functions/elliptic-functions.js
+++ src/functions/elliptic-functions.js
@@ -342,6 +342,20 @@ function weierstrassRoots( g2, g3 ) {
 
 }
 
+function weierstrassHalfPeriods( g2, g3 ) {
+
+  var [ e1, e2, e3 ] = weierstrassRoots( g2, g3 );
+
+  var w1 = inverseWeierstrassP( e1, g2, g3 );
+  var w2 = inverseWeierstrassP( e2, g2, g3 );
+  var w3 = inverseWeierstrassP( e3, g2, g3 );
+
+  return [ w1, w2, w3 ];
+
+}
+
+
+
 function weierstrassP( x, g2, g3 ) {
 
   if ( !isComplex(x) ) x = complex(x);
